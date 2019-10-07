bd9e8b53e4d553311e16761b91f74b2799912f37
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-07 09:50:31 +0100

    06 events registering listener and emitters

--- first_app/app.js
+++ first_app/app.js
@@ -1,7 +1,9 @@
 const path = require('path'); //build in module no ./
-const log  = require('./logger'); // custom module has ./
+const log = require('./logger'); // custom module has ./
 const os = require('os');
 const fs = require('fs'); // all either sync/blocking or async/non-blocking
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 log('ivanao');
 
@@ -13,15 +15,22 @@ console.log(`total memmory ${totalMem}`);
 
 let totalFreeMem = os.freemem();
 console.log(`free memory ${totalFreeMem}`);
-
 //sync should be avoided, mostly used for simiplicity ... blocking is the enemy!?
 const files = fs.readdirSync('./');
 console.log(`sync files: ${files}`);
 
 //all async take a function as last param // did somebody say call back?
-const asyncFiles = fs.readdir('./',function(error, files){
+const asyncFiles = fs.readdir('./', function (error, files) {
   error ?
     console.error(`Error: ${error}`) :
     console.log(`async files: ${files}`);
-})
+});
 
+// Register a listener
+emitter.on('messageLogged', function () {
+  console.log('listener called');
+});
+// raise an event singmalling
+setTimeout(() => {
+  emitter.emit('messageLogged');
+}, 500)
