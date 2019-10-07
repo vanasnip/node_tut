662f6d3d965fea3f8dcde51c54f93e8f3d30f34a
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-07 06:37:53 +0100

    05 fs module, use async because its non-blocking, avoid sync; async methods take a callback as the last param with error first handling

--- first_app/app.js
+++ first_app/app.js
@@ -1,6 +1,7 @@
 const path = require('path'); //build in module no ./
 const log  = require('./logger'); // custom module has ./
 const os = require('os');
+const fs = require('fs'); // all either sync/blocking or async/non-blocking
 
 log('ivanao');
 
@@ -12,3 +13,15 @@ console.log(`total memmory ${totalMem}`);
 
 let totalFreeMem = os.freemem();
 console.log(`free memory ${totalFreeMem}`);
+
+//sync should be avoided, mostly used for simiplicity ... blocking is the enemy!?
+const files = fs.readdirSync('./');
+console.log(`sync files: ${files}`);
+
+//all async take a function as last param // did somebody say call back?
+const asyncFiles = fs.readdir('./',function(error, files){
+  error ?
+    console.error(`Error: ${error}`) :
+    console.log(`async files: ${files}`);
+})
+
