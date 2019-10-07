1e619cfaf9ab9b471901cb0e0d66ca5a84a59f08
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-07 05:40:34 +0100

    04 looked at some os functions

--- first_app/app.js
+++ first_app/app.js
@@ -1,3 +1,14 @@
-const log  = require('./logger');
+const path = require('path'); //build in module no ./
+const log  = require('./logger'); // custom module has ./
+const os = require('os');
 
 log('ivanao');
+
+let pathObj = path.parse(__filename);
+console.log(pathObj);
+
+let totalMem = os.totalmem();
+console.log(`total memmory ${totalMem}`);
+
+let totalFreeMem = os.freemem();
+console.log(`free memory ${totalFreeMem}`);
