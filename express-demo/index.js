34b4cc5c95fe6c104dcac7b590e4775a576a1b02
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-08 13:40:12 +0100

    15 added query string for optional params

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,10 +11,18 @@ app.get('/api/courses', (req,res)=>{
 });
 
 app.get('/api/courses/:id',(req,res)=>{
-  let id = req.params.id;
+  let {id} = req.params;
   res.send(id);
-  console.log(`logging message: ${id}`);
+  console.log(`logging message: id - ${id}`);
 });
+app.get('/api/courses/:year/:month',(req,res)=>{
+  let {year, month} = req.params; // params required
+  let query = req.query; //opitonal
+  res.send(query);
+  console.log(`logging message: year - ${year} | month - ${month} | query - ${query || 'none'}`);
+});
+
+
 
 // PORT
 const port = process.env.PORT || 3000;
