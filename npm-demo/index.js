ea9ca16fd2e0cb32c8bb0ec915a409cc10ea9614
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-08 09:10:07 +0100

    13 npm, installing, semver, versioning, updating, publishing packages

---
+++ npm-demo/index.js
@@ -0,0 +1,5 @@
+const _ = require('./node_modules/underscore'); // assumes core module // file of folder // then checks node modules
+
+const check = _.contains([1,2,3,4],2);
+console.log(check);
+
---
+++ test-my-package/index.js
@@ -0,0 +1,3 @@
+const { add } = require('simple-add-function');
+
+console.log(add(9,1));
