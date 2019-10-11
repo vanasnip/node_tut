5f4d0dfe26d8c51a4d4c922ca0fb5e7c94648e31
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-11 12:14:57 +0100

    02 put custom middleware in separate files and required

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,14 +1,15 @@
 
 const express = require('express');
+const logger = require('./logger');
+const authenticate = require('./auth');
 const app = express();
 const Joi = require('joi');
 
 app.use(express.json());
 
-app.use((req, res, next)=>{
-  console.log(`${new Date()} - ${req.method} - ${req.url}`);
-  next();
-})
+app.use(logger);
+
+app.use();
 
 const courses = [
   { id: 1, name: 'course1' },
---
+++ express-demo/logger.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next)=>{
+  console.log(`${new Date()} - ${req.method} - ${req.url}`);
+  next();
+}
+
+
