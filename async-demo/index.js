7d7917b5526f5e57847466f6f83f031bfd9cd96b
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-15 03:39:33 +0100

    02 added example of callback hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,12 +1,26 @@
+//Callbacks
+
 console.log('Before');
-const user = getUser(1);
-console.log(user); // undefined
+getUser(1,(user)=>{
+  getRepos(user.githubUsername, (repos)=>{ // the begining of call bak hell, then handling for potential error for each
+    console.log(repos);
+  })
+}); // called when setTimeout runs, effectively saying I'm done);
+//console.log(user); // undefined
 console.log('After');
 
-function getUser(id) {
+function getUser(id, callback) {
   const githubUsername = 'ivano';
   setTimeout(() => {
     console.log(`Reading a user from database with id ${id}`);
-    return {id, githubUsername}
+    callback( {id, githubUsername});
+  }, 2000);
+}
+
+function getRepos(username, callback){
+  setTimeout(()=>{
+    let repos = ['repo1', 'repo2', 'repo3', 'repo4'];
+    console.log(`Calling github api`);
+    callback({username, repos});
   }, 2000);
 }
