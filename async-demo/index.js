e53d019df0a47813a84570bc20d558169c454811
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-15 05:26:45 +0100

    05 changed flattened callback structure into promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,42 +1,51 @@
 //Callbacks
 
 
-getUser(1, displayUser);
+getUser(1)
+  .then(({ githubUsername: user }) => getRepos(user))
+  .then(({ repos }) => getCommits(repos[0]))
+  .then(({ commits }) => console.log(`finally got the commits I was after...`))
+  .catch(err => console.error(err.message));
 
-function displayUser(user) {
-  console.log(user);
-  getRepos(user.githubUsername, displayRepos)
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Date.now() % 3 === 0) {
+        reject(new Error(`error getting user`));
+      } else {
+        const user = { id, githubUsername: 'ivano' };
+        console.log(`Managed to get user ${user.githubUsername}`);
+        resolve(user);
+      }
+    }, 2000);
+  })
 }
 
-function displayRepos(repos) {
-  // the begining of call bak hell, then handling for potential error for each
-  console.log(repos);
-  getCommits(repos[0], displayCommits)
+function getRepos(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Date.now() % 3 === 0) {
+        reject(new Error(`error getting repos`));
+      } else {
+        let repos = ['repo1', 'repo2', 'repo3', 'repo4'];
+        console.log(`Managed to get repos ${repos}`);
+        resolve({ username, repos });
+      }
+    }, 2000);
+  });
 }
-
-function displayCommits(commits) {
-  console.log(commits)
-}
-
-function getUser(id, callback) {
-  const githubUsername = 'ivano';
-  setTimeout(() => {
-    console.log(`Reading a user from database with id ${id}`);
-    callback({ id, githubUsername });
-  }, 2000);
-}
-
-function getRepos(username, callback) {
-  setTimeout(() => {
-    let repos = ['repo1', 'repo2', 'repo3', 'repo4'];
-    console.log(`Calling github api`);
-    callback({ username, repos });
-  }, 2000);
-}
-
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    let commits = ['commit1', 'commit2', 'commit3', 'commit4']
-    callback(commits);
-  }, 2000);
+//
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Date.now() % 3 === 0) {
+        reject(new Error(`error getting commits`));
+      } else {
+        let commits = ['commit1', 'commit2', 'commit3', 'commit4']
+        console.log(`Managed to get commits ${commits}`);
+        resolve({ commits });
+      }
+    }, 2000);
+  });
 }
