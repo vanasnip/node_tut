b43f4ea4becb401a52cadbda8292bc7a830480a5
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-17 16:12:07 +0100

    08 added logical operators to handle the filters

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -32,18 +32,17 @@ async function CreateCourse() {
 //CreateCourse();
 
 async function getCourses(){
-  // eq -> equal
-  // ne -> not equal
-  // gt -> greater than
-  // gte -> greater than or equal to
-  // lt -> less than
-  // lte -> less than or equal to
-  // in
-  // nin -> not in
+  // eq -> equal // ne -> not equal // gt -> greater than // gte -> greater than or equal to
+  // lt -> less than // lte -> less than or equal to // in // nin -> not in
+
+  // or
+  // and
+
   const courses = await Course
   // .find({ author: 'Ivano', isPublished: true})
-  // .find({ price: { $gt: 10, $lte:20}})
-  .find({ price: {$in: [10,15,20]}})
+  .find()
+  .or([{author: 'Ivano'},{isPublished: true}])
+  .and([{name:'Angular'},{}])
   .limit(10)
   .sort({name: 1})
   .select({ name: 1, tags: 1});
