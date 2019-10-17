3f5050ec22478ce1d53f12fcb99125c954fea885
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2019-10-17 11:11:54 +0100

    02 added mongoose schema which doesnt exist in mongo, its just  way of telling mongo the shape of the documents we want in that collection

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,11 @@ mongoose.connect('mongodb://localhost/playground') // this conenct method return
 .then(()=> console.log(`Conencted to MongoDB...`))
 .catch(err => console.error(`Could not connect to the MongoDB... ${err}`));
 
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
